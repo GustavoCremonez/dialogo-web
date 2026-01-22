e1f3199533c918b72573da1c5e12c7c7562c7372
GustavoCremonez <gustavocremonezgc@gmail.com>
2026-01-22 07:28:50 -0300

    chore: configura roteamente e remove arquivo de boiterplate

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: () => import('../views/LandingPage/LandingPage.vue') },
+    { path: '/login', component: () => import('../views/UserLogin.vue') },
+  ],
 })
 
 export default router
